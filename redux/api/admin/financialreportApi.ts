bb2d30c2752680e2151cba2ae4bcda78db7a3cbd
MD Ahsanul Hasan Rifat <ahsanul@softvence.com>
2026-04-28 11:25:36 +0600

    financial report part all done only the arpu growth factor remaining

--- redux/api/admin/financialreportApi.ts
+++ redux/api/admin/financialreportApi.ts
@@ -31,6 +31,83 @@ export const financialreportApi = baseApi.injectEndpoints({
         params,
       }),
     }),
+    getChurnStats: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/churn/stats`,
+        method: "GET",
+        params,
+      }),
+    }),
+    getChurnTrend: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/churn/trend`,
+        method: "GET",
+        params,
+      }),
+    }),
+    getChurnRateByPlan: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/churn/rate-by-plan`,
+        method: "GET",
+        params,
+      }),
+    }),
+    getPlanStats: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/plans/stats`,
+        method: "GET",
+        params,
+      }),
+    }),
+    getPlanRevenue: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/plans/revenue`,
+        method: "GET",
+        params,
+      }),
+    }),
+    getPlanSubscribers: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/plans/subscribers`,
+        method: "GET",
+        params,
+      }),
+    }),
+    getTrialStats: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/trail/stats`,
+        method: "GET",
+        params,
+      }),
+    }),
+    getTrialConvertByPlan: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/trail/convert-by-plan`,
+        method: "GET",
+        params,
+      }),
+    }),
+    getArpuStats: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/arpu/stats`,
+        method: "GET",
+        params,
+      }),
+    }),
+    getArpuTrend: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/arpu/trend`,
+        method: "GET",
+        params,
+      }),
+    }),
+    getExportFinancialReport: builder.query({
+      query: (params) => ({
+        url: `/reports-analytics/financial/export-financial-report`,
+        method: "GET",
+        params,
+      }),
+    }),
   }),
 });
 
@@ -39,4 +116,16 @@ export const {
   useGetMrrStatsQuery,
   useGetMrrTrendQuery,
   useGetFinancialBreakdownQuery,
+  useGetChurnStatsQuery,
+  useGetChurnTrendQuery,
+  useGetChurnRateByPlanQuery,
+  useGetPlanStatsQuery,
+  useGetPlanRevenueQuery,
+  useGetPlanSubscribersQuery,
+  useGetTrialStatsQuery,
+  useGetTrialConvertByPlanQuery,
+  useGetArpuStatsQuery,
+  useGetArpuTrendQuery,
+  useGetExportFinancialReportQuery,
+  useLazyGetExportFinancialReportQuery,
 } = financialreportApi;
